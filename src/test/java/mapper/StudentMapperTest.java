d525c64ab43090685f9a8d5d46558e8ec29c61ea
毛 <1296193245@qq.com>
2022-04-05 21:36:35 +0800

    Mybatis_Plus

---
+++ src/test/java/mapper/StudentMapperTest.java
@@ -0,0 +1,91 @@
+package mapper;
+
+import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
+import data.Student;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project name(项目名称)：Mybatis_Plus
+ * Package(包名): mapper
+ * Class(测试类名): StudentMapperTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/4/5
+ * Time(创建时间)： 21:12
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ */
+
+class StudentMapperTest
+{
+
+    @Test
+    void findAll() throws IOException
+    {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new
+                SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> list = studentMapper.findAll();
+        for (Student student : list)
+        {
+            System.out.print(student);
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    void findAll1() throws IOException
+    {
+        //GenericTypeUtils默认使用了Spring的GenericTypeResolver来作为泛型工具助手
+        String resource = "mybatis-config.xml";
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream(resource);
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+        List<Student> list = studentMapper.selectList(null);
+        for (Student student : list)
+        {
+            System.out.print(student);
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    void findById() throws IOException
+    {
+        //GenericTypeUtils默认使用了Spring的GenericTypeResolver来作为泛型工具助手
+        String resource = "mybatis-config.xml";
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream(resource);
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        Student student1 = studentMapper.selectById(202012340101L);
+        System.out.println(student1);
+
+        sqlSession.close();
+    }
+
+}
